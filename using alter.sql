24730e37c0e6e1188f712d377e9aec44dc0f4a69
vidhya002 <37336805+vidhya002@users.noreply.github.com>
2018-04-30 12:24:39 +0530

    Update using alter.sql

--- using alter.sql
+++ using alter.sql
@@ -10,4 +10,5 @@ create table user(
 insert into user values("160","SRI","20"),("154","JO","20"),("215","SANDHU","15");
 select user_name,user_age from user where user_age >="20";
 ALTER table user add user_native char(100);
+ALTER table user drop user_age ;
 desc user;
